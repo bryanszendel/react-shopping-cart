e64957bfe0cad03b548283d7ecc7bff2fd448b08
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-12 14:32:14 -0600

    product component refactored in app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import { ProductContext } from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -20,21 +23,20 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<ProductContext.Provider value={{ products, addItem }}>
+			  <Route
+  				exact
+  				path="/"
+  				component={products}
+  			/>
+			</ProductContext.Provider>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<ProductContext.Provider value={{ products, addItem}}>
+			  <Route
+  				path="/cart"
+  				render={() => <ShoppingCart cart={cart} />}
+  			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
